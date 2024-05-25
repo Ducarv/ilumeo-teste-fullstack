391f5673b18c321eb2e33f3d4ca9f24397d5bc44
ducarv <educarvalho009@gmail.com>
2024-05-25 12:48:15 -0400

    style: user controller index

--- backend/src/controllers/user/index.ts
+++ backend/src/controllers/user/index.ts
@@ -1,12 +1,12 @@
-import { UserRepository } from "../../repositories/implementation/prismaImpl/UserRepository";
-import { CreateUserUseCase } from "../../useCases/user/create";
-import { FindUserByIdUseCase } from "../../useCases/user/findById";
-import { GetAllUsersUseCase } from "../../useCases/user/getAll";
-import { CreateUserController } from "./create.controller";
-import { FindUseByIdController } from "./findById.controller";
-import { GetAllUsersController } from "./getAll.controller";
+import { UserRepository } from '../../repositories/implementation/prismaImpl/UserRepository';
+import { CreateUserUseCase } from '../../useCases/user/create';
+import { FindUserByIdUseCase } from '../../useCases/user/findById';
+import { GetAllUsersUseCase } from '../../useCases/user/getAll';
+import { CreateUserController } from './create.controller';
+import { FindUseByIdController } from './findById.controller';
+import { GetAllUsersController } from './getAll.controller';
 
-const prismaRepository = new UserRepository()
+const prismaRepository = new UserRepository();
 
 const createUserUseCase = new CreateUserUseCase(prismaRepository);
 const createUserController = new CreateUserController(createUserUseCase);
@@ -17,4 +17,4 @@ const findUserByIdController = new FindUseByIdController(findUserByIdUseCase);
 const getAllUsersUseCase = new GetAllUsersUseCase(prismaRepository);
 const getAllUsersController = new GetAllUsersController(getAllUsersUseCase);
 
-export { createUserController, findUserByIdController, getAllUsersController }
+export { createUserController, findUserByIdController, getAllUsersController };
